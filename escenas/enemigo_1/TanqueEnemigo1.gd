7d0c62a15e27d7a00640a161f58e278ba2ee4b68
Lujan <wasabisoundandmusic@gmail.com>
2025-10-30 19:27:56 -0300

    arreglado vision de los tanques

--- escenas/enemigo_1/TanqueEnemigo1.gd
+++ escenas/enemigo_1/TanqueEnemigo1.gd
@@ -13,25 +13,22 @@ func _physics_process(delta):
 	else:
 		movimiento = Vector2.ZERO
 	movimiento = movimiento.normalized() * velocidad
-	movimiento = move_and_slide(movimiento)
+	
+	#Rotacion del sprite
+	
+	if movimiento.length() > 0.1 and $Sprite:
+		# Calcula el ángulo hacia donde se mueve
+		var angulo = movimiento.angle() + PI/2 # Ajustá el offset si tu sprite mira hacia arriba 
+		
+		$Sprite.rotation = angulo
+		
+	move_and_slide(movimiento)
 
-
-<<<<<<< Updated upstream
 func _on_Area2D_body_entered(body): #Con este puede perseguir al tanque del jugador, falta ajustar direccion del sprite
 	if body != self:
 		jugador = body
-=======
-func _on_Area2D_body_entered(body): #Con esta area detecta al enemigo para acercarse a él
-	if body != self:
-		jugador = body
-	
->>>>>>> Stashed changes
 
 
 func _on_Area2D_body_exited(body):
 	jugador = null
-<<<<<<< Updated upstream
 
-=======
-	
->>>>>>> Stashed changes
