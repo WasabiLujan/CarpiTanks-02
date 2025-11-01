f85f0e43b1559451d5ceffed6e84be8f2f83971f
Lujan <wasabisoundandmusic@gmail.com>
2025-10-31 21:36:12 -0300

    actualizacion verdolaga

--- escenas/PJ/Projectile.gd
+++ escenas/PJ/Projectile.gd
@@ -1,35 +1,26 @@
 extends KinematicBody2D
 
-# velocidad en px/s
-export var velocidad = 700
-# tiempo de vida en segundos
-export var lifetime = 2.0
+var movimiento = Vector2.ZERO
 
-# dirección que le pasamos desde el tanque (Vector2)
-var direccion = Vector2.ZERO
-
-var _tiempo = 0.0
+var velocidad = 300
+var angulo = 0
 
 func _ready():
-	# Conectamos señales para destruir el proyectil al chocar
-	connect("area_entered", self, "_on_area_entered")
-	connect("body_entered", self, "_on_body_entered")
+	angulo = get_angle_to(get_global_mouse_position())
+	
+	movimiento.x = cos(angulo)
+	movimiento.y = sin(angulo)
 
-func _process(delta):
-	# Movimiento simple por posición (no physics)
-	if direccion != Vector2.ZERO:
-		position += direccion.normalized() * velocidad * delta
-	else:
-		# fallback: moverse en la dirección del rotation si no se pasó direccion
-		position += Vector2(cos(rotation), sin(rotation)) * velocidad * delta
+func _physics_process(delta):
+	
+	movimiento = movimiento.normalized() * velocidad
+	
+	movimiento = move_and_slide(movimiento)
+	#aca quede
 
-	_tiempo += delta
-	if _tiempo >= lifetime:
-		queue_free()
+func _on_Timer_timeout():
+	self.queue_free()
 
-func _on_area_entered(area):
-	# podes filtrar grupos si hace falta (ej: "enemigo"), ahora simplemente se destruye
-	queue_free()
 
-func _on_body_entered(body):
-	queue_free()
+func _on_zona_colicion_body_entered(body):
+	self.queue_free()
--- escenas/PJ/Tanque.gd
+++ escenas/PJ/Tanque.gd
@@ -3,6 +3,14 @@ extends KinematicBody2D
 var velocidad = 200
 var movimiento = Vector2()
 #var ultipo_movimiento = #Para cargar en él el ultimo movimiento que se realizó, y usarlo para que el
+const proyectil_objeto = preload("res://escenas/PJ/Projectile.tscn")
+
+func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		var proyectil = proyectil_objeto.instance()
+		get_parent().add_child(proyectil)
+	
+
 
 func _physics_process(delta):
 	
@@ -36,3 +44,5 @@ func movimiento():
 func nitro():
 	if Input.is_action_pressed("shoot"):
 		velocidad = 1000
+
+
