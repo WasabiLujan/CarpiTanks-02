c677eff7d9facc3b300c0608a43a6bf66d636ece
Lujan <wasabisoundandmusic@gmail.com>
2025-11-17 03:38:54 -0300

    Ajustes del mov del aliado + Refo del Nvl + Expacion del area2d de la piedra
    El aliado se mueve un poco más intelijente
    Pendiente: Que el mov sea mas suave e inteligente
    Refo del nivel
    Area2D de la piedra rompible arreglada
    Pasaje secreto agregado

--- escenas/Aliados/aliado.gd
+++ escenas/Aliados/aliado.gd
@@ -1,25 +1,78 @@
 extends KinematicBody2D
 
 var motion = Vector2.ZERO
-var velocity = 100
+var velocity = 150
 
 var jugador_cerca = false
 var jugador = null
 var detenerse = false
 
-var colicionar_con_mundo = false
+#var colicionar_con_mundo = false
+
+func _ready():
+	pass
 
 func _physics_process(delta):
 	motion = Vector2.ZERO
 	
-	if jugador_cerca == true and detenerse == false:
+	#Aca revisamos si la exitencia del jugador existe pero fue eliminada, en el caso del Game Over
+	if jugador and not is_instance_valid(jugador):#Investigar sobre el "is_intance_valid"
+		jugador = null
+		jugador_cerca = false
+	
+	#Aca seguimos al jugador, con varias condiciones para que no de un bug
+	if jugador and is_instance_valid(jugador) and not detenerse:
+		#Almacenamos en una variable la accion de seguir al jugador
+		var dire = position.direction_to(jugador.position)
+		#Aca usamos los Ray para detectar si choca algo y corregir la direccion
+		# Cuando coliciona arriba
+		if dire.y < -0.4 and $RayCastUp.is_colliding():
+			dire.y = 0 #Deja de seguir intentando ir hacia arriba al chocar en esa dire
+		#Abajo
+		if dire.y > 0.4 and $RayCastDown.is_colliding():
+			dire.y = 0
+		#Izquierda
+		if dire.x < -0.4 and $RayCastLeft.is_colliding():
+			dire.x = 0
+		#Derecha
+		if dire.x > 0.4 and $RayCastRigth.is_colliding():
+			dire.x = 0
+		#Si ambos componentes quedan en 0, estamos bloqueados
+		if dire != Vector2.ZERO:
+			motion = dire.normalized() * velocity
+		# Diagonales
+		if dire.x > 0.4 and dire.y < -0.4 and $RayCastUpRight.is_colliding():
+			dire.x = 0
+			dire.y = 0
+		
+		if dire.x > 0.4 and dire.y > 0.4 and $RayCastDownRight.is_colliding():
+			dire.x = 0
+			dire.y = 0
+		
+		if dire.x < -0.4 and dire.y < -0.4 and $RayCastUpLeft.is_colliding():
+			dire.x = 0
+			dire.y = 0
+		
+		if dire.x < -0.4 and dire.y > 0.4 and $RayCastDownLeft.is_colliding():
+			dire.x = 0
+			dire.y = 0
+	else:
+		motion = Vector2.ZERO
+	
+	movimiento_aliado() #Actualizamos visual del tanque dependiendo direccion resultante
+		
+	move_and_slide(motion)
+	
+	
+	"""
+	if jugador_cerca and not detenerse:
 		motion = position.direction_to(jugador.position)
 		movimiento_aliado()
 
 	
 	motion = motion.normalized() * velocity
 	move_and_slide(motion)
-	
+	"""
 
 func movimiento_aliado():
 	var x = motion.x
@@ -49,15 +102,22 @@ func _on_AreaDeActivacion_body_entered(body):
 	if body.is_in_group("Jugador"):
 		jugador = body
 		jugador_cerca = true
-		
+	
+
+func _on_AreaDeActivacion_body_exited(body):
+	#if body == jugador:
+		#jugador_cerca = false
+		#jugador = null
+	pass
 
 
 func _on_AreaParaDetenerce_body_entered(body):
 	if body.is_in_group("Jugador"):
-		detenerse = true
 		jugador = body
+		detenerse = true
 		motion = Vector2.ZERO
 
 func _on_AreaParaDetenerce_body_exited(body):
 	if body.is_in_group("Jugador"):
 		detenerse = false
+
